7e4435addb7e5ec71d4752b9ce06adf8b7b881e5
Laoise Fitzpatrick <laoise.fitzpatrick@gmail.com>
2017-12-15 15:11:47 +0000

    Added basic contact form functionality, changed design

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$from = 'From: Contact Form'; 
+$to = 'erin@tfwno.gf'; 
+$subject = 'Website Contact Form';
+
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+}
+?>
